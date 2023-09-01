2e835f1fb2724c2b48a8c9fb7603f21c90303c31
luizeduardosb <luizeduardo00736@gmail.com>
2023-09-01 09:14:45 -0300

    Apps 'empresas' e 'users'

--- itjc/settings.py
+++ itjc/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'paginas',
     'editais',
+    'empresas',
+    'users',
 ]
 
 MIDDLEWARE = [
--- itjc/urls.py
+++ itjc/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('paginas.urls')),
-    path('editais/', include('editais.urls'))
+    path('editais/', include('editais.urls')),
+    path('empresas/', include('empresas.urls')),
 ]
