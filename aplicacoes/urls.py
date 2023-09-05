e53b11c6d56c1c9b6996238eac5947f9fb48eaa8
luizeduardosb <luizeduardo00736@gmail.com>
2023-09-05 15:34:41 -0300

    Listagem das empresas fail

--- aplicacoes/urls.py
+++ aplicacoes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.editais, name='editais'),
     path('edital/<int:id>', views.editalView, name='edital-view'),
+    path('', views.empresas, name='empresas'),
 ]
--- aplicacoes/views.py
+++ aplicacoes/views.py
@@ -6,8 +6,15 @@ def home(request):
     editais = Edital.objects.all()
     return render(request, 'aplicacoes/home.html', {'editais': editais})
 
+def editais(request):
+    editais = Edital.objects.all()
+    return render(request, 'aplicacoes/home.html/#editais', {'editais': editais})
 
 def editalView(request, id):
     edital = get_object_or_404(Edital, pk=id)
     return render(request, 'aplicacoes/edital.html', {'edital': edital})
+
+def empresas(request):
+    empresas = Empresa.objects.all()
+    return render(request, 'aplicacoes/home.html/#empresas', {'empresas': empresas})
     
