77d5193a5d8e0e87ef96979bef4589ab90451700
luizeduardosb <luizeduardo00736@gmail.com>
2023-09-05 15:17:05 -0300

    Listagem e detalhamento dos editais

--- aplicacoes/urls.py
+++ aplicacoes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('edital/<int:id>', views.editalView, name='edital-view'),
 ]
--- aplicacoes/views.py
+++ aplicacoes/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Edital
+from .models import Empresa
 
 def home(request):
-    return render(request, 'aplicacoes/home.html')
+    editais = Edital.objects.all()
+    return render(request, 'aplicacoes/home.html', {'editais': editais})
+
+
+def editalView(request, id):
+    edital = get_object_or_404(Edital, pk=id)
+    return render(request, 'aplicacoes/edital.html', {'edital': edital})
+    
