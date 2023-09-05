ca62ec0ac0820d4e164e9a6addf76f06ae2d8de0
luizeduardosb <luizeduardo00736@gmail.com>
2023-09-05 14:10:06 -0300

    Att área administrativa

--- aplicacoes/models.py
+++ aplicacoes/models.py
@@ -7,6 +7,8 @@ class Edital(models.Model):
     titulo = models.CharField(max_length=150)
     subtitulo = models.CharField(max_length=250)
     materia = models.TextField()
+    link = models.TextField(null=True)
+    inscricao = models.TextField(null=True)
     status = models.CharField(max_length=9, choices=STATUS,)
 
     crated_at = models.DateTimeField(auto_now_add=True)
@@ -14,11 +16,18 @@ class Edital(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+    class Meta:
+        verbose_name_plural = 'Editais'
 
 class Empresa(models.Model):
 
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
-    localizacao = models.CharField(max_length=200)
-    site = models.CharField(max_length=100)
-    contato = models.TextField()
+    localizacao = models.CharField(max_length=200, null=True, blank=True)
+    endereco_digital = models.CharField(max_length=100, null=True, blank=True)
+    telefone = models.CharField(max_length=50, null=True, blank=True)
+    whatsapp = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
