893f9b0886941803f93fa898c92923d668e111e4
Maria Aparecidade Da silva de souza <wilkerjadson000@gmail.com>
2023-10-02 11:48:16 -0300

    Função menu corrigda definitivamente

--- aplicacoes/static/js/scripts.js
+++ aplicacoes/static/js/scripts.js
@@ -3,21 +3,18 @@ function openMenu() {
   dropdown.classList.toggle('open'); // qnd a função é chamada a classe é adicionada 
 }
 
-var menuIcon = document.querySelector('.drop');
-menuIcon.addEventListener('click', openMenu); // monitoramento do botão .drop
-
-
 document.addEventListener('click', function (event) {
   var dropdown = document.querySelector('.dropdown');
   var menuIcon = document.querySelector('.drop');
 
   if (!dropdown.contains(event.target) && !menuIcon.contains(event.target)) {
-    openMenu();
+    var dropdown = document.querySelector('.dropdown');
+    dropdown.classList.remove('open');
   }// monitorando algum click fora do menu para fechar ele
 
   if (event.target.tagName === 'P' && dropdown.contains(event.target)) {
     openMenu();
   }// monitorando algum click em uma tag p
-}); 
+});
 
 
